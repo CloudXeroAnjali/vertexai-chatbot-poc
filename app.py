90dede16e60fa8c44625482426af689720cfefa1
CloudXeroAnjali <160321793+CloudXeroAnjali@users.noreply.github.com>
2024-02-18 22:15:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ from vertexai.language_models import ChatModel
 import os
 
 app = Flask(__name__)
+PROJECT_ID = "vertex-ai-chatbot-414313"  
+LOCATION = "us-central1"  
+
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+def create_session():
+    chat_model = ChatModel.from_pretrained("chat-bison@001")
+    chat = chat_model.start_chat()
+    return chat
+
+def response(chat, message):
+    parameters = {
+        "temperature": 0.2,
+        "max_output_tokens": 256,
+        "top_p": 0.8,
+        "top_k": 40
+    }
+    result = chat.send_message(message, **parameters)
+    return result.text
 
 @app.route('/')
 def index():
