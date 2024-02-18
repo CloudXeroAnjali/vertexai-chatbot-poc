89966aa5d8c005b5899b877ecbec5d3a840cb334
CloudXeroAnjali <160321793+CloudXeroAnjali@users.noreply.github.com>
2024-02-18 22:59:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ LOCATION = "us-central1"
 DATA_STORE_ID = "vertex-ai-chatbot-dataset_1708011349777"  # @param {type:"string"}
 DATA_STORE_LOCATION = "global"  # @param {type:"string"}
 REGION = "us-central1"  # @param {type:"string"}
-MODEL = "chat-bison@001"  # @param {type:"string"}
+MODEL = "text-bison@001"  # @param {type:"string"}
 
 os.environ["DATA_STORE_ID"] = DATA_STORE_ID
 os.environ["PROJECT_ID"] = PROJECT_ID
@@ -23,16 +23,6 @@ from langchain.retrievers import GoogleVertexAISearchRetriever
 
 vertexai.init(project=PROJECT_ID, location=REGION)
 llm = VertexAI(model_name=MODEL)
-
-retriever = GoogleVertexAISearchRetriever(
-    project_id=PROJECT_ID,
-    location_id=DATA_STORE_LOCATION,
-    data_store_id=DATA_STORE_ID,
-    get_extractive_answers=True,
-    max_documents=40,
-    max_extractive_segment_count=1,
-    max_extractive_answer_count=5,
-)
 
 @app.route('/')
 def index():
@@ -48,7 +38,7 @@ def vertex_palm():
     
     #result = conversational_retrieval({"question": user_input})
     #content = response(chat_model,user_input)
-    return jsonify(content=user_input)
+    return jsonify(content='')
 
 if __name__ == '__main__':
     app.run(debug=True)#, port=8000, host='0.0.0.0')
