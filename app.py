f2f53f078a9a627e9d341624ae5aef540413050d
CloudXeroAnjali <160321793+CloudXeroAnjali@users.noreply.github.com>
2024-02-18 22:57:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -34,33 +34,6 @@ retriever = GoogleVertexAISearchRetriever(
     max_extractive_answer_count=5,
 )
 
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-from langchain.retrievers import GoogleVertexAIMultiTurnSearchRetriever
-
-multi_turn_retriever = GoogleVertexAIMultiTurnSearchRetriever(
-    project_id=PROJECT_ID, location_id=DATA_STORE_LOCATION, data_store_id=DATA_STORE_ID
-)
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-conversational_retrieval = ConversationalRetrievalChain.from_llm(
-    llm=llm, retriever=multi_turn_retriever, memory=memory
-)
-
-def create_session():
-    chat_model = ChatModel.from_pretrained("chat-bison@001")
-    chat = chat_model.start_chat()
-    return chat
-
-def response(chat, message):
-    parameters = {
-        "temperature": 0.2,
-        "max_output_tokens": 256,
-        "top_p": 0.8,
-        "top_k": 40
-    }
-    result = chat.send_message(message, **parameters)
-    return result.text
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -72,11 +45,10 @@ def vertex_palm():
         user_input = request.args.get('user_input')
     else:
         user_input = request.form['user_input']
-    chat_model = create_session()
     
-    result = conversational_retrieval({"question": user_input})
+    #result = conversational_retrieval({"question": user_input})
     #content = response(chat_model,user_input)
-    return jsonify(content=result)
+    return jsonify(content=user_input)
 
 if __name__ == '__main__':
     app.run(debug=True)#, port=8000, host='0.0.0.0')
